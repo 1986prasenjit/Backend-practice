962ee4eec5954b762906da6d80900f1460400b41
1986prasenjit <prasenjitbasu44@gmail.com>
2025-07-04 17:54:57 +0530

    added user model

--- full-stack-practice/src/models/User.model.js
+++ full-stack-practice/src/models/User.model.js
@@ -1,6 +1,42 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
+    name:{
+        type:String,
+        lowercase:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+    },
+    isUserVerified:{
+        type:Boolean,
+        default:false
+    },
+    role:{
+        type:String,
+        enum:["USER","ADMIN"],
+        default:"USER"
+    },
+    refreshToken:{
+        type:String
+    },
+    passwordResetToken:{
+        type:String,
+    },
+    passwordResetExpiry:{
+        type:Date,
+    }
 
 })
 
